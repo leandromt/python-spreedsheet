6429e3a22b926722c716adb8812be468b095a970
Leandro Tavares <leandro.mendestavares@gmail.com>
2020-01-16 11:46:57 -0300

    Add function write xlsx

--- app.py
+++ app.py
@@ -1,3 +1,25 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
-xl.
+def process_workbook(filename):
+    wb = xl.load_workbook(filename=filename)
+    sheet = wb['Sheet1']
+
+    # Each rows
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        correct_price = cell.value * 0.9
+        correct_price_cell = sheet.cell(row, 4)
+        correct_price_cell.value = correct_price
+
+    values = Reference(sheet,
+              min_row=2,
+              max_row=sheet.max_row,
+              min_col=4,
+              max_col=4)
+
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
+
+    wb.save(filename)
---
+++ machinelearning.py
@@ -0,0 +1,2 @@
+# Machine Learning Steps
+# 1 - Import Data
