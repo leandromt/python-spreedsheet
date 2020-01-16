d11565b82e1c139048aa93bacdfcec8a0570c6f4
Leandro Tavares <leandro.mendestavares@gmail.com>
2020-01-16 13:38:26 -0300

    Add Tips to Machine Learning

--- machinelearning.py
+++ machinelearning.py
@@ -1,2 +1,18 @@
 # Machine Learning Steps
 # 1 - Import Data
+# 2 - Clena Data
+# 3 - Split the data into Training / Tests Sets (Percents, 50%)
+# 4 - Create a Model
+# 5 - Train the model
+# 6 - Make Predictions
+# 7 - Enveluate and Improve
+
+# Libraries
+# 1 - Numpy
+# 2 - Panda
+# 3 - MatPlotLib
+# 4 - Scikit-Learn
+
+# Pesquisar Sobre:
+# https://jupyter.org/
+# anaconda
